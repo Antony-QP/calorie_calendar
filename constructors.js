ed4227c108cbc1e4b6381139adeada41b843583a
Antony Parker <antonyqparker@gmail.com>
2020-08-07 16:15:14 +0200

    nearly fully functioning except cannot delete data from local storage yet

--- constructors.js
+++ constructors.js
@@ -1,8 +1,19 @@
 const form = document.querySelector("#form-info");
-const tableData = document.querySelector('#table-data')
+const tableData = document.querySelector("#table-data");
+let idCounter = 0;
 
 // Post constructor
-function Post(sex, name, activity, age, allowance, breakfast, lunch, dinner) {
+function Post(
+  sex,
+  name,
+  activity,
+  age,
+  allowance,
+  breakfast,
+  lunch,
+  dinner,
+  id
+) {
   this.sex = sex;
   this.name = name;
   this.activity = activity;
@@ -11,83 +22,163 @@ function Post(sex, name, activity, age, allowance, breakfast, lunch, dinner) {
   this.breakfast = breakfast;
   this.lunch = lunch;
   this.dinner = dinner;
+  this.id = idCounter++;
 }
 
 // UI CONSTRUCTOR
 function UI() {}
 
-// UI PROTOTYPES 
+// UI PROTOTYPES
 UI.prototype.addPostToList = function (post) {
-  console.log(post);
   // Create table row
-  const tableRow = document.createElement('tr');
+  const tableRow = document.createElement("tr");
   // Log Date
   let today = new Date();
-  today = today.toDateString()
+  today = today.toDateString();
   // Calculate total calorie consumption
-  let totalCal = parseFloat(breakfast) + parseFloat(lunch) + parseFloat(dinner);
+  let totalCal =
+    parseFloat(post.breakfast) +
+    parseFloat(post.lunch) +
+    parseFloat(post.dinner);
   // Use API to check calorie intake (next step)
 
   // Append table row to table
-  tableRow.innerHTML =
-    `
+  tableRow.innerHTML = `
     <td>${today}</td>
     <td>${totalCal}</<td>
+    <td id="postid">${post.id}</<td>
     <td><a href="#" class="delete">X</a></td>
-    `
-  tableData.appendChild(tableRow)
-}
+    `;
+  tableData.appendChild(tableRow);
+};
 
 // Delete Row Function
 UI.prototype.deleteRow = function (target) {
-  if (target.className === 'delete') {
+  if (target.className === "delete") {
     target.parentElement.parentElement.remove();
   }
-}
+  // console.log(target.parentElement.parentElement.childNodes[4])
+};
 
 // Show alert function
 UI.prototype.showAlert = function (message, className) {
   // Create a div
-  const div = document.createElement('div');
+  const div = document.createElement("div");
   // Add a class name
   div.className = `alert ${className}`;
   // Add text to alert
   div.appendChild(document.createTextNode(message));
   // Get parent
-  const container = document.querySelector('.container');
-  const formContainer = document.querySelector('.form-container');
-  // Insert before form 
+  const container = document.querySelector(".container");
+  const formContainer = document.querySelector(".form-container");
+  // Insert before form
   container.insertBefore(div, formContainer);
-}
+  // Set timeout so message dissapears
+  setTimeout(function () {
+    document.querySelector(".alert").remove();
+  }, 3000);
+};
 
-form.addEventListener('submit', function (e) {
-  const sex = document.querySelector('#sex').value;
-  const name = document.querySelector('#name').value;
-  const activity = document.querySelector('#activity').value;
-  const age = document.querySelector('#age').value;
-  const allowance = document.querySelector('#allowance').value;
-  const breakfast = document.querySelector('#breakfast').value;
-  const lunch = document.querySelector('#lunch').value;
-  const dinner = document.querySelector('#dinner').value;
+// Event listener for form submission
+form.addEventListener("submit", function (e) {
+  const sex = document.querySelector("#sex").value;
+  const name = document.querySelector("#name").value;
+  const activity = document.querySelector("#activity").value;
+  const age = document.querySelector("#age").value;
+  const allowance = document.querySelector("#allowance").value;
+  const breakfast = document.querySelector("#breakfast").value;
+  const lunch = document.querySelector("#lunch").value;
+  const dinner = document.querySelector("#dinner").value;
 
-
-  const post = new Post(sex, name, activity, age, allowance, breakfast, lunch, dinner);
+  const post = new Post(
+    sex,
+    name,
+    activity,
+    age,
+    allowance,
+    breakfast,
+    lunch,
+    dinner
+  );
 
   const ui = new UI();
 
-  if (sex === '' || name === '' || activity === '' || age === '' || allowance === '' || breakfast === '' || lunch === '' || dinner === '') {
-    ui.showAlert("Please fill in all fields", "error")
+  if (
+    sex === "" ||
+    name === "" ||
+    activity === "" ||
+    age === "" ||
+    allowance === "" ||
+    breakfast === "" ||
+    lunch === "" ||
+    dinner === ""
+  ) {
+    ui.showAlert("Please fill in all fields", "error");
   } else {
     ui.addPostToList(post);
+    Store.addPost(post);
+    ui.showAlert("Calorie intake successfully added to counter", "success");
   }
-})
+  e.preventDefault();
+});
+
+// Storing to local storage
+
+class Store {
+  // Get Posts
+  static getPosts() {
+    let posts;
+    if (localStorage.getItem("posts") === null) {
+      posts = [];
+    } else {
+      posts = JSON.parse(localStorage.getItem("posts"));
+    }
+    return posts;
+  }
+
+  // Add Post
+  static addPost(post) {
+    const posts = Store.getPosts();
+
+    posts.push(post);
+
+    localStorage.setItem("posts", JSON.stringify(posts));
+  }
+
+  // Display posts
+  static displayPosts() {
+    const posts = Store.getPosts();
+    posts.forEach(function (post) {
+      const ui = new UI();
+      ui.addPostToList(post);
+    });
+  }
+
+  // Remove posts
+  static removePost() {
+    const posts = Store.getPosts();
+
+    posts.forEach(function (post, index) {
+      if (post.id === index) {
+        console.log(post.id);
+        posts.splice(post.id, 1);
+      }
+    });
+    localStorage.setItem('posts', JSON.stringify(posts))
+  }
+}
+
+// Event listener to display posts
+document.addEventListener("DOMcontentLoaded", Store.displayPosts());
 
 // Event Listener for deleteRow
-document.getElementById('table-data').addEventListener('click', function (e) {
+document.getElementById("table-data").addEventListener("click", function (e) {
   // Instatiate new UI so that you can access the prototypes
-  const ui = new UI;
+  const ui = new UI();
   // Delete book
   ui.deleteRow(e.target);
-  console.log("Row deleted");
+  ui.showAlert("Day has been deleted", "success");
+  Store.removePost();
+  // Remove from local storage
   e.preventDefault();
-})
+});
